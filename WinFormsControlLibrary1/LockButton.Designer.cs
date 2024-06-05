732bb7adf0308b2c83aa83967039d01d5b948baf
alisaozerova <155026323+alisaozerova@users.noreply.github.com>
2024-06-05 23:15:30 +0300

    Add files via upload

---
+++ WinFormsControlLibrary1/LockButton.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace WinFormsControlLibrary1
+{
+    [ToolboxBitmap(@"C:\Users\user\source\repos\.NET\Lab_1\WinFormsControlLibrary1\LockButton.ico")]
+    partial class LockButton : Button
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer = new System.Windows.Forms.Timer(components);
+            
+            timer.Interval = 1000;
+
+            timer.Tick += (sender, e) =>
+            {
+                LockButtonTick();
+            };
+
+            Click += (sender, e) =>
+            {
+                LockButtonClick();
+            };
+        }
+        #endregion
+    }
+}
---
+++ WinFormsControlLibrary1/LockButton.cs
@@ -0,0 +1,51 @@
+﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace WinFormsControlLibrary1
+{
+    public partial class LockButton : Button
+    {
+        private System.Windows.Forms.Timer timer;
+        private int delay;
+
+        private String caption;
+        private int time;
+
+        public int Delay
+        {
+            get { return delay; }
+            set
+            {
+                delay = value;
+            }
+        }
+
+        private void LockButtonClick()
+        {
+            caption = Text;
+            time = delay;
+            Enabled = false;
+            Text = "Wait " + time + " sec";
+            timer.Start();
+        }
+
+        private void LockButtonTick()
+        {
+            time--;
+            if (time == 0)
+            {
+                Enabled = true;
+                Text = caption;
+                timer.Stop();
+            }
+            else {
+                Text = "Wait " + time + " sec";
+            }
+        }
+
+        public LockButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
